fbb443e9cd10362f88504b424575a27924457280
Wei Xin Koh <e0201350@u.nus.edu>
2020-03-31 20:52:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function addToCart(item) {
  // returns a random integer from 1 to 100
  var price = Math.floor(Math.random() * 100) + 1;
 
- itemInfo[itemName] = item;
- itemInfo[itemPrice] = price;
+ itemInfo['itemName'] = item;
+ itemInfo['itemPrice'] = price;
 
  cart.push(itemInfo);
 
